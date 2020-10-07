276846cef3521bb65646feb84b0b5ff86173d0ba
Moro-Code <omar@omarnasr.ca>
2020-10-07 12:06:38 -0400

    template repository

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, {useEffect} from 'react';
+import './App.css';
+import {useTranslation} from "react-i18next";
+import {useSelector} from "react-redux";
+import Routes from "./router";
+
+function App() {
+  // i18n
+  let {i18n} = useTranslation();
+
+  // state subscription to language
+  const language = useSelector(state => state.language)
+
+  // effect to handle language switching based on redux state
+  useEffect(
+      () => {
+        if(i18n.language !== language){
+            i18n.changeLanguage(language)
+        }
+      },
+      [
+          i18n,
+          language
+      ]
+  )
+
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
