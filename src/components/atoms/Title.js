de5cbc349ac4162a8572bdb0bf79007a506818de
shewood <shewood01@gmail.com>
2020-10-20 13:19:53 -0400

    for title component

--- src/components/atoms/Title.js
+++ src/components/atoms/Title.js
@@ -1,14 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Title(props) {
-  console.log(props);
-  return <h1>{props.title}</h1>;
+/**
+ * Displays the title on the page
+ */
+
+export function Title(props) {
+  return <h1>{props.children}</h1>;
 }
 
 Title.propTypes = {
   /**
-   * Text area that displays the title on the page
+   * Text area that displays the title for the page
    */
-  title: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
 };
