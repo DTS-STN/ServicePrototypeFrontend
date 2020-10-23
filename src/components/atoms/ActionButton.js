f6bdaf08fbf43a31aca4da3edb3862e2c5007e7d
shawn320 <azuneko1110@gmail.com>
2020-10-23 16:01:32 -0400

    story and test

--- src/components/atoms/ActionButton.js
+++ src/components/atoms/ActionButton.js
@@ -5,16 +5,35 @@ import PropTypes from "prop-types";
  * Button component which is used to trigger actions on the page
  */
 export function ActionButton(props) {
+  /**
+   * This function checks if the button is a filter button
+   */
+  const isFilterButton = (type, count) => {
+    if (type === "filter")
+      return (
+        <div className="ml-4 rounded-full h-6 w-6 flex item-center justify-center bg-gray-300 text-gray-700">
+          {count}
+        </div>
+      );
+  };
+
   return (
     <button
       className={`flex justify-center content-center h-auto w-auto p-1 ${
         props.rounded ? "rounded-full py-2 px-4" : "rounded-md"
-      } shadow-lg bg-gray-700 text-white text-sm hover:bg-black hover:bg-black`}
+      } shadow-lg ${
+        props.invert
+          ? "bg-gray-100 text-gray-700 border border-gray-400 px-4 hover:bg-gray-700 hover:text-white focus:outline-none"
+          : "bg-gray-700 text-white hover:bg-black"
+      } text-sm`}
       onClick={props.onClick}
       onMouseEnter={props.onHover}
       onMouseLeave={props.onMouseLeave}
     >
-      {props.text}
+      <div className="flex">
+        {props.text}
+        {isFilterButton(props.type, props.count)}
+      </div>
     </button>
   );
 }
@@ -24,6 +43,16 @@ ActionButton.propTypes = {
    * The text that the button will display
    */
   text: PropTypes.string.isRequired,
+
+  /**
+   * Specifies the type of button
+   */
+  type: PropTypes.string,
+
+  /**
+   * Display benefits count
+   */
+  count: PropTypes.number,
 
   /**
    * Boolean flag that specifies the button should be rounded
--- src/components/molecules/BenefitFilter.js
+++ src/components/molecules/BenefitFilter.js
@@ -1,11 +1,71 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { ActionButton } from "../atoms/ActionButton";
 
-export function BenefitFilter(prop) {
+/**
+ * This component displays three navigation buttons which filters out the result of benefits
+ */
+export function BenefitFilter(props) {
   return (
     <div>
-      <p>{props.text}</p>
-      <ActionButton text={props.eligibleBenefits} />
+      <p className="text-gray-700 leading-none font-bold">{props.text}</p>
+      <div className="flex w-9/12">
+        <ActionButton
+          type="filter"
+          text={props.eligible}
+          count={props.eligibleCount}
+          invert={true}
+        />
+        <ActionButton
+          type="filter"
+          text={props.help}
+          count={props.helpCount}
+          invert={true}
+        />
+        <ActionButton
+          type="filter"
+          text={props.others}
+          count={props.othersCount}
+          invert={true}
+        />
+      </div>
     </div>
   );
 }
+
+BenefitFilter.propTypes = {
+  /**
+   * Text on top of the buttons
+   */
+  text: PropTypes.string,
+
+  /**
+   * Button text that displays Eligible benefits
+   */
+  eligible: PropTypes.string,
+
+  /**
+   * Displays the number of results of eligible benefits
+   */
+  eligibleCount: PropTypes.number,
+
+  /**
+   * Button text that displays Potential help
+   */
+  help: PropTypes.string,
+
+  /**
+   * Displays the number of results of potential help
+   */
+  helpCount: PropTypes.number,
+
+  /**
+   * Other filter options that may be applied
+   */
+  others: PropTypes.string,
+
+  /**
+   * Display the number of results of other option that may applies
+   */
+  othersCount: PropTypes.number,
+};
