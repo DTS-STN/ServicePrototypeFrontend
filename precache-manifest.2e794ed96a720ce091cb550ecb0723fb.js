f8e9712bb32e7ef356eb0cd43899231b91d484a8
Moro-Code <omar@omarnasr.ca>
2020-12-15 17:49:55 +0000

    Deploying to gh-pages from  @ a393d6f4ff671e7f51460643181ef78dea63d99c 🚀

---
+++ precache-manifest.2e794ed96a720ce091cb550ecb0723fb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea7fcd491ac9257e8358977788d3058f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.b27806926410f8038f93.bundle.js"
+  },
+  {
+    "url": "runtime~main.b27806926410f8038f93.bundle.js"
+  },
+  {
+    "revision": "638a45e389b71e717eea",
+    "url": "static/css/main.41603e58.chunk.css"
+  },
+  {
+    "revision": "d43eb968e25e81ce0af629bad1069a19",
+    "url": "static/media/code-brackets.d43eb968.svg"
+  },
+  {
+    "revision": "8104e6f5a95cbe9c9615b47daf881fc7",
+    "url": "static/media/colors.8104e6f5.svg"
+  },
+  {
+    "revision": "3f9a1398f6372bd43806b239c70f4759",
+    "url": "static/media/comments.3f9a1398.svg"
+  },
+  {
+    "revision": "d3dc567bd3e26a031bfd7fab1e765508",
+    "url": "static/media/direction.d3dc567b.svg"
+  },
+  {
+    "revision": "e3812c4ff33e4e7ae9686a7fcfef393f",
+    "url": "static/media/flow.e3812c4f.svg"
+  },
+  {
+    "revision": "1e1cc496db37d78a3d4a481e6a1e9e87",
+    "url": "static/media/icomoon.1e1cc496.eot"
+  },
+  {
+    "revision": "513fc48146543d12f4d170e71bc5c5e9",
+    "url": "static/media/icomoon.513fc481.woff"
+  },
+  {
+    "revision": "782a1d5b0d6762fd687ee027ec3e4104",
+    "url": "static/media/icomoon.782a1d5b.svg"
+  },
+  {
+    "revision": "c97b9c99efbb0f1db623f99e12b57c4e",
+    "url": "static/media/icomoon.c97b9c99.ttf"
+  },
+  {
+    "revision": "5ba97e9be21511d352f0056b1970ce21",
+    "url": "static/media/plugin.5ba97e9b.svg"
+  },
+  {
+    "revision": "3452b916238c37efec707e6979db3556",
+    "url": "static/media/repo.3452b916.svg"
+  },
+  {
+    "revision": "26c1aa4d9e0022fccc0f9390ec961e50",
+    "url": "static/media/sig-blk-en.26c1aa4d.svg"
+  },
+  {
+    "revision": "66f4b748553f89f290a3247a913072af",
+    "url": "static/media/stackalt.66f4b748.svg"
+  },
+  {
+    "revision": "c4cb91c963cba579b6332b7a64f70f93",
+    "url": "static/media/wmms-blk.c4cb91c9.svg"
+  },
+  {
+    "url": "vendors~main.b27806926410f8038f93.bundle.js"
+  },
+  {
+    "url": "vendors~main.b27806926410f8038f93.bundle.js.LICENSE.txt"
+  }
+]);
