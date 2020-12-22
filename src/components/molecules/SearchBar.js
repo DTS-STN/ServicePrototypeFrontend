4d40b9dcc87a3f0695484c5d63f6f001cd858bc0
shawn320 <azuneko1110@gmail.com>
2020-12-22 12:04:35 -0500

    react-responsive

--- src/components/molecules/SearchBar.js
+++ src/components/molecules/SearchBar.js
@@ -1,25 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { ActionButton } from "../atoms/ActionButton";
+import { useMediaQuery } from "react-responsive";
 
 /**
  * The search bar component that allow user to search keyword to show matching result of benefits
  */
 export function SearchBar(props) {
   const handleClick = () => props.onSearch();
+  /**
+   * Check if screen size is mobile
+   */
+  const isMobile = useMediaQuery({ query: `(max-width: 516px)` });
 
   return (
     <div className="md:bg-gray-md md:flex items-center p-2 px-4">
       <p className="hidden md:flex text-lg">{props.text}</p>
       <div className="md:ml-8 flex w-auto">
-        <i className="icon-search p-2" />
+        {/**
+         <i className="icon-search p-2" />
+         */}
         <input
-          type="text"
-          placeholder={props.placeholder}
           className="w-full md:w-72 rounded-l-full text-center focus:outline-none border-t border-b border-l border-gray-md"
+          type="text"
+          value={props.value}
+          placeholder={props.placeholder}
         />
         <ActionButton
-          text={props.buttonText}
+          text={isMobile ? "" : props.buttonText}
           halfRound={true}
           className="shadow-none bg-gray-light border border-gray-md"
           icon2="icon-cheveron-down p-1"
