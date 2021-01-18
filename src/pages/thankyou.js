ec51d6b61bcf2db55c275006a610bbb53efaec50
Thomas Dacey <DaceyTom2@gmail.com>
2021-01-18 11:00:00 -0500

    Added barbones thank you page

---
+++ src/pages/thankyou.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+//component imports
+import { ContentPage } from "../components/organisms/ContentPage";
+
+export function ThankYouPage() {
+  return <ContentPage content={"Thank you for applying"} />;
+}
