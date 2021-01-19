acd5f65a8851dedea4117c36edb56c95ff25bc96
Thomas Dacey <DaceyTom2@gmail.com>
2021-01-19 10:28:10 -0500

    Added translations to thank you page

--- src/pages/thankyou.js
+++ src/pages/thankyou.js
@@ -1,8 +1,20 @@
 import React from "react";
 
+// i18n imports
+import { useTranslation } from "react-i18next";
+
+// component imports
+import { Title } from "../components/atoms/Title";
 //component imports
 import { ContentPage } from "../components/organisms/ContentPage";
 
 export function ThankYouPage() {
-  return <ContentPage content={"Thank you for applying"} />;
+  const { t } = useTranslation();
+
+  return (
+    <ContentPage
+      beforeContent={<Title>{t("thankYouTitle")}</Title>}
+      content={t("thankYouContent")}
+    />
+  );
 }
