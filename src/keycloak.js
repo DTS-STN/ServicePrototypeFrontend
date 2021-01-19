a0be9b4ad703996c879442259a5299cfda622ccf
MarcoGoC <marco.flores@hrsdc-rhdcc.gc.ca>
2021-01-19 10:42:54 -0500

    update to the keycloak setup file

--- src/keycloak.js
+++ src/keycloak.js
@@ -1,12 +1,13 @@
 import Keycloak from "keycloak-js";
+import { KEYCLOAK_URL, KEYCLOAK_REALM, KEYCLOAK_CLIENTID } from "./variables";
 
 // Setup Keycloak instance as needed
 // Pass initialization options as required  TODO
 
 const keycloak = new Keycloak({
-  url: "https://keycloak.dev.dts-stn.com/auth",
-  realm: "benefit-service-dev",
-  clientId: "benefit-service-frontend",
+  url: KEYCLOAK_URL,
+  realm: KEYCLOAK_REALM,
+  clientId: KEYCLOAK_CLIENTID,
 });
 
 export default keycloak;
--- src/variables.js
+++ src/variables.js
@@ -11,10 +11,26 @@ if (!benefitservice_env_url && ENVIRONMENT !== "production") {
 
 export const BENEFITSERVICE_URL = benefitservice_env_url;
 
-// Keycloak service
-export const keycloak_env_url = process.env.KEYCLOAK_URL;
-export const keycloak_env_realm = process.env.KEYCLOAK_REALM;
-export const keycloak_env_clientId = process.env.KEYCLOAK_CLIENTID;
+//
+// variables for the Keycloak service
+//
+let keycloak_env_url = process.env.KEYCLOAK_URL;
+let keycloak_env_realm = process.env.KEYCLOAK_REALM;
+let keycloak_env_clientId = process.env.KEYCLOAK_CLIENTID;
+
+if (!keycloak_env_url && ENVIRONMENT !== "production") {
+  keycloak_env_url = "https://keycloak.dev.dts-stn.com/auth";
+}
+if (!keycloak_env_realm && ENVIRONMENT !== "production") {
+  keycloak_env_realm = "benefit-service-dev";
+}
+if (!keycloak_env_clientId && ENVIRONMENT !== "production") {
+  keycloak_env_clientId = "benefit-service-frontend";
+}
+
+export const KEYCLOAK_URL = keycloak_env_url;
+export const KEYCLOAK_REALM = keycloak_env_realm;
+export const KEYCLOAK_CLIENTID = keycloak_env_clientId;
 
 // This is for the benefit-service url
 
