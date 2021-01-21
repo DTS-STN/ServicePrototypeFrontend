a3b8cde65b7e107bad6f606d979e9269ae9ef633
shewood <shewood01@gmail.com>
2021-01-21 15:46:21 -0500

    updated tests

--- cypress/integration/benefit-details.spec.js
+++ cypress/integration/benefit-details.spec.js
@@ -5,24 +5,36 @@
 describe("Benefits cards spec", () => {
   
     beforeEach(() => { 
-    //  cy.visit("/");
-      cy.intercept( {method: 'Get', url: 'http://localhost:3000/benefit/1'}, { fixture: "descriptionEN.json" })
-     //.as("getDescriptEN")
+      cy.visit("/");
+     
       ;
     });
 
 
+    it.only("Mocked number for benefit Count appears in the UI", () => {
+        // The number of the benefits is returned from the server, this number is being mocked here
+        cy.intercept("/benefits/*", { fixture: "descriptionFR.json" }).as("getCount");
+  
+        cy.visit("http://localhost:3000/benefit/1");
+  
+        // pass an array of Route Aliases that forces Cypress to wait
+        // each of these aliases   cy.wait(['@getBenefits', '@getCount'])
+        cy.wait(["@getCount"]);
+        // these commands will not run until the wait command resolves above
+        cy.contains('mocked')
+      //  cy.get("[data-cy=home-page-benefit-counter]").should("be.visible").and("contain.text", "4");
+      });
 
 
-      it.only("Should show the Benefit details page", () => {
+      it("Should show the Benefit details page", () => {
         // the benefit text is returned from the server, the text here is being mocked
-     //    cy.intercept( {method: 'Get', url: '**benefit/1'}, { fixture: "descriptionEN.json" }).as("getDescriptEN");
+         cy.intercept( {pathname: '/benefits*'}, { fixture: "descriptionEN.json" }).as("getDescriptEN");
      //    cy.wait(2000)
       //   cy.screenshot('1page-before')
-      //   cy.visit("/benefit/1");
+         cy.visit("/benefit/1");
        //  cy.wait(2000)
       //   cy.screenshot('1page-after')
-      //   cy.wait("@getDescriptEN");
+        cy.wait("@getDescriptEN");
         // these commands will not run until the wait command resolves above
         //  cy.get("[data-cy-button=more-info-button]").should("be.visible")
         //  cy.get("[data-cy-button=more-info-button]").should('have.length', 3)
