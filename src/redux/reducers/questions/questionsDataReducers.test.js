43f5d5cab77908541a741fbcbc1e164b42644e49
Benoit Cloutier <cloutier.ben@yahoo.com>
2021-01-29 15:27:54 -0500

    Finished the questions reducer testing.

--- src/redux/reducers/questions/questionsDataReducers.test.js
+++ src/redux/reducers/questions/questionsDataReducers.test.js
@@ -2,8 +2,6 @@ import {
   networkRequestActionCreator,
   networkReceivedActionCreator,
   networkRequestFailedActionCreator,
-  selectBenefitActionCreator,
-  deselectBenefitActionCreator,
   NETWORK_FAILED_REASONS,
   NETWORK_REQUEST_TYPES,
 } from "../../actions";
@@ -86,7 +84,7 @@ describe("questionsData", () => {
         },
       },
       networkReceivedActionCreator(
-        RESOURCE_TYPES.BENEFITS,
+        RESOURCE_TYPES.QUESTIONS,
         NETWORK_REQUEST_TYPES.GET,
         [
           {
@@ -162,409 +160,33 @@ describe("questionsData", () => {
       },
     });
   });
-  // it("handles network request failed action", () => {
-  //   const result = questionsData(
-  //     {
-  //       isFetching: true,
-  //       fetchFailed: false,
-  //       fetchFailedReason: "",
-  //       fetchFailedObj: {},
-  //       questionsMap: {},
-  //       benefitsKeyToIdMap: {},
-  //     },
-  //     networkRequestFailedActionCreator(
-  //       RESOURCE_TYPES.BENEFITS,
-  //       NETWORK_REQUEST_TYPES.GET,
-  //       NETWORK_FAILED_REASONS.INTERNAL_SERVER_ERROR,
-  //       {
-  //         some: "key",
-  //       }
-  //     )
-  //   );
+  it("handles network request failed action", () => {
+    const result = questionsData(
+      {
+        isFetching: true,
+        fetchFailed: false,
+        fetchFailedReason: "",
+        fetchFailedObj: {},
+        questionsMap: {},
+      },
+      networkRequestFailedActionCreator(
+        RESOURCE_TYPES.QUESTIONS,
+        NETWORK_REQUEST_TYPES.GET,
+        NETWORK_FAILED_REASONS.INTERNAL_SERVER_ERROR,
+        {
+          some: "key",
+        }
+      )
+    );
 
-  //   expect(result).toEqual({
-  //     isFetching: false,
-  //     fetchFailed: true,
-  //     fetchFailedReason: NETWORK_FAILED_REASONS.INTERNAL_SERVER_ERROR,
-  //     fetchFailedObj: {
-  //       some: "key",
-  //     },
-  //     questionsMap: {},
-  //     benefitsKeyToIdMap: {},
-  //   });
-  // });
-  // it("handles benefit select action", () => {
-  //   const result = questionsData(
-  //     {
-  //       isFetching: true,
-  //       fetchFailed: true,
-  //       fetchFailedReason: "sdfsdfsdfs",
-  //       fetchFailedObj: {
-  //         some: "key",
-  //       },
-  //       questionsMap: {
-  //         1: {
-  //           id: 1,
-  //           title_en: "Employment Insurance (EN)",
-  //           title_fr: "Employment Insurance Benefit (FR)",
-  //           title_es: null,
-  //           created_by: {
-  //             id: 1,
-  //             firstname: "admin",
-  //             lastname: "user",
-  //             username: null,
-  //           },
-  //           updated_by: {
-  //             id: 1,
-  //             firstname: "admin",
-  //             lastname: "user",
-  //             username: null,
-  //           },
-  //           created_at: "2020-11-24T16:21:56.055Z",
-  //           updated_at: "2020-11-24T16:21:56.090Z",
-  //           benefit_key: "ei_benefit",
-  //           benefit_description_en:
-  //             "This benefit lays out what employment insurance looks like (EN)",
-  //           benefit_description_fr:
-  //             "This benefit lays out what employment insurance looks like (FR)",
-  //           benefit_content_en: null,
-  //           benefit_content_fr: null,
-  //           benefit_link_en: null,
-  //           benefit_link_fr: null,
-  //           isEligible: false,
-  //           isSelected: false,
-  //         },
-  //       },
-  //       benefitsKeyToIdMap: {
-  //         ei_benefit: 1,
-  //       },
-  //     },
-  //     selectBenefitActionCreator(1)
-  //   );
-
-  //   expect(result).toEqual({
-  //     isFetching: true,
-  //     fetchFailed: true,
-  //     fetchFailedReason: "sdfsdfsdfs",
-  //     fetchFailedObj: {
-  //       some: "key",
-  //     },
-  //     questionsMap: {
-  //       1: {
-  //         id: 1,
-  //         title_en: "Employment Insurance (EN)",
-  //         title_fr: "Employment Insurance Benefit (FR)",
-  //         title_es: null,
-  //         created_by: {
-  //           id: 1,
-  //           firstname: "admin",
-  //           lastname: "user",
-  //           username: null,
-  //         },
-  //         updated_by: {
-  //           id: 1,
-  //           firstname: "admin",
-  //           lastname: "user",
-  //           username: null,
-  //         },
-  //         created_at: "2020-11-24T16:21:56.055Z",
-  //         updated_at: "2020-11-24T16:21:56.090Z",
-  //         benefit_key: "ei_benefit",
-  //         benefit_description_en:
-  //           "This benefit lays out what employment insurance looks like (EN)",
-  //         benefit_description_fr:
-  //           "This benefit lays out what employment insurance looks like (FR)",
-  //         benefit_content_en: null,
-  //         benefit_content_fr: null,
-  //         benefit_link_en: null,
-  //         benefit_link_fr: null,
-  //         isEligible: false,
-  //         isSelected: true,
-  //       },
-  //     },
-  //     benefitsKeyToIdMap: {
-  //       ei_benefit: 1,
-  //     },
-  //   });
-
-  //   const resultForOFKey = questionsData(
-  //     {
-  //       isFetching: true,
-  //       fetchFailed: true,
-  //       fetchFailedReason: "sdfsdfsdfs",
-  //       fetchFailedObj: {
-  //         some: "key",
-  //       },
-  //       questionsMap: {
-  //         1: {
-  //           id: 1,
-  //           title_en: "Employment Insurance (EN)",
-  //           title_fr: "Employment Insurance Benefit (FR)",
-  //           title_es: null,
-  //           created_by: {
-  //             id: 1,
-  //             firstname: "admin",
-  //             lastname: "user",
-  //             username: null,
-  //           },
-  //           updated_by: {
-  //             id: 1,
-  //             firstname: "admin",
-  //             lastname: "user",
-  //             username: null,
-  //           },
-  //           created_at: "2020-11-24T16:21:56.055Z",
-  //           updated_at: "2020-11-24T16:21:56.090Z",
-  //           benefit_key: "ei_benefit",
-  //           benefit_description_en:
-  //             "This benefit lays out what employment insurance looks like (EN)",
-  //           benefit_description_fr:
-  //             "This benefit lays out what employment insurance looks like (FR)",
-  //           benefit_content_en: null,
-  //           benefit_content_fr: null,
-  //           benefit_link_en: null,
-  //           benefit_link_fr: null,
-  //           isEligible: false,
-  //           isSelected: false,
-  //         },
-  //       },
-  //       benefitsKeyToIdMap: {
-  //         ei_benefit: 1,
-  //       },
-  //     },
-  //     selectBenefitActionCreator("ei_benefit")
-  //   );
-
-  //   expect(result).toEqual({
-  //     isFetching: true,
-  //     fetchFailed: true,
-  //     fetchFailedReason: "sdfsdfsdfs",
-  //     fetchFailedObj: {
-  //       some: "key",
-  //     },
-  //     questionsMap: {
-  //       1: {
-  //         id: 1,
-  //         title_en: "Employment Insurance (EN)",
-  //         title_fr: "Employment Insurance Benefit (FR)",
-  //         title_es: null,
-  //         created_by: {
-  //           id: 1,
-  //           firstname: "admin",
-  //           lastname: "user",
-  //           username: null,
-  //         },
-  //         updated_by: {
-  //           id: 1,
-  //           firstname: "admin",
-  //           lastname: "user",
-  //           username: null,
-  //         },
-  //         created_at: "2020-11-24T16:21:56.055Z",
-  //         updated_at: "2020-11-24T16:21:56.090Z",
-  //         benefit_key: "ei_benefit",
-  //         benefit_description_en:
-  //           "This benefit lays out what employment insurance looks like (EN)",
-  //         benefit_description_fr:
-  //           "This benefit lays out what employment insurance looks like (FR)",
-  //         benefit_content_en: null,
-  //         benefit_content_fr: null,
-  //         benefit_link_en: null,
-  //         benefit_link_fr: null,
-  //         isEligible: false,
-  //         isSelected: true,
-  //       },
-  //     },
-  //     benefitsKeyToIdMap: {
-  //       ei_benefit: 1,
-  //     },
-  //   });
-  // });
-  // it("handles benefit deselect action", () => {
-  //   const result = questionsData(
-  //     {
-  //       isFetching: true,
-  //       fetchFailed: true,
-  //       fetchFailedReason: "sdfsdfsdfs",
-  //       fetchFailedObj: {
-  //         some: "key",
-  //       },
-  //       questionsMap: {
-  //         1: {
-  //           id: 1,
-  //           title_en: "Employment Insurance (EN)",
-  //           title_fr: "Employment Insurance Benefit (FR)",
-  //           title_es: null,
-  //           created_by: {
-  //             id: 1,
-  //             firstname: "admin",
-  //             lastname: "user",
-  //             username: null,
-  //           },
-  //           updated_by: {
-  //             id: 1,
-  //             firstname: "admin",
-  //             lastname: "user",
-  //             username: null,
-  //           },
-  //           created_at: "2020-11-24T16:21:56.055Z",
-  //           updated_at: "2020-11-24T16:21:56.090Z",
-  //           benefit_key: "ei_benefit",
-  //           benefit_description_en:
-  //             "This benefit lays out what employment insurance looks like (EN)",
-  //           benefit_description_fr:
-  //             "This benefit lays out what employment insurance looks like (FR)",
-  //           benefit_content_en: null,
-  //           benefit_content_fr: null,
-  //           benefit_link_en: null,
-  //           benefit_link_fr: null,
-  //           isEligible: false,
-  //           isSelected: true,
-  //         },
-  //       },
-  //       benefitsKeyToIdMap: {
-  //         ei_benefit: 1,
-  //       },
-  //     },
-  //     deselectBenefitActionCreator(1)
-  //   );
-
-  //   expect(result).toEqual({
-  //     isFetching: true,
-  //     fetchFailed: true,
-  //     fetchFailedReason: "sdfsdfsdfs",
-  //     fetchFailedObj: {
-  //       some: "key",
-  //     },
-  //     questionsMap: {
-  //       1: {
-  //         id: 1,
-  //         title_en: "Employment Insurance (EN)",
-  //         title_fr: "Employment Insurance Benefit (FR)",
-  //         title_es: null,
-  //         created_by: {
-  //           id: 1,
-  //           firstname: "admin",
-  //           lastname: "user",
-  //           username: null,
-  //         },
-  //         updated_by: {
-  //           id: 1,
-  //           firstname: "admin",
-  //           lastname: "user",
-  //           username: null,
-  //         },
-  //         created_at: "2020-11-24T16:21:56.055Z",
-  //         updated_at: "2020-11-24T16:21:56.090Z",
-  //         benefit_key: "ei_benefit",
-  //         benefit_description_en:
-  //           "This benefit lays out what employment insurance looks like (EN)",
-  //         benefit_description_fr:
-  //           "This benefit lays out what employment insurance looks like (FR)",
-  //         benefit_content_en: null,
-  //         benefit_content_fr: null,
-  //         benefit_link_en: null,
-  //         benefit_link_fr: null,
-  //         isEligible: false,
-  //         isSelected: false,
-  //       },
-  //     },
-  //     benefitsKeyToIdMap: {
-  //       ei_benefit: 1,
-  //     },
-  //   });
-
-  //   const resultForOFKey = questionsData(
-  //     {
-  //       isFetching: true,
-  //       fetchFailed: true,
-  //       fetchFailedReason: "sdfsdfsdfs",
-  //       fetchFailedObj: {
-  //         some: "key",
-  //       },
-  //       questionsMap: {
-  //         1: {
-  //           id: 1,
-  //           title_en: "Employment Insurance (EN)",
-  //           title_fr: "Employment Insurance Benefit (FR)",
-  //           title_es: null,
-  //           created_by: {
-  //             id: 1,
-  //             firstname: "admin",
-  //             lastname: "user",
-  //             username: null,
-  //           },
-  //           updated_by: {
-  //             id: 1,
-  //             firstname: "admin",
-  //             lastname: "user",
-  //             username: null,
-  //           },
-  //           created_at: "2020-11-24T16:21:56.055Z",
-  //           updated_at: "2020-11-24T16:21:56.090Z",
-  //           benefit_key: "ei_benefit",
-  //           benefit_description_en:
-  //             "This benefit lays out what employment insurance looks like (EN)",
-  //           benefit_description_fr:
-  //             "This benefit lays out what employment insurance looks like (FR)",
-  //           benefit_content_en: null,
-  //           benefit_content_fr: null,
-  //           benefit_link_en: null,
-  //           benefit_link_fr: null,
-  //           isEligible: false,
-  //           isSelected: true,
-  //         },
-  //       },
-  //       benefitsKeyToIdMap: {
-  //         ei_benefit: 1,
-  //       },
-  //     },
-  //     deselectBenefitActionCreator("ei_benefit")
-  //   );
-
-  //   expect(result).toEqual({
-  //     isFetching: true,
-  //     fetchFailed: true,
-  //     fetchFailedReason: "sdfsdfsdfs",
-  //     fetchFailedObj: {
-  //       some: "key",
-  //     },
-  //     questionsMap: {
-  //       1: {
-  //         id: 1,
-  //         title_en: "Employment Insurance (EN)",
-  //         title_fr: "Employment Insurance Benefit (FR)",
-  //         title_es: null,
-  //         created_by: {
-  //           id: 1,
-  //           firstname: "admin",
-  //           lastname: "user",
-  //           username: null,
-  //         },
-  //         updated_by: {
-  //           id: 1,
-  //           firstname: "admin",
-  //           lastname: "user",
-  //           username: null,
-  //         },
-  //         created_at: "2020-11-24T16:21:56.055Z",
-  //         updated_at: "2020-11-24T16:21:56.090Z",
-  //         benefit_key: "ei_benefit",
-  //         benefit_description_en:
-  //           "This benefit lays out what employment insurance looks like (EN)",
-  //         benefit_description_fr:
-  //           "This benefit lays out what employment insurance looks like (FR)",
-  //         benefit_content_en: null,
-  //         benefit_content_fr: null,
-  //         benefit_link_en: null,
-  //         benefit_link_fr: null,
-  //         isEligible: false,
-  //         isSelected: false,
-  //       },
-  //     },
-  //     benefitsKeyToIdMap: {
-  //       ei_benefit: 1,
-  //     },
-  //   });
-  // });
+    expect(result).toEqual({
+      isFetching: false,
+      fetchFailed: true,
+      fetchFailedReason: NETWORK_FAILED_REASONS.INTERNAL_SERVER_ERROR,
+      fetchFailedObj: {
+        some: "key",
+      },
+      questionsMap: {},
+    });
+  });
 });
