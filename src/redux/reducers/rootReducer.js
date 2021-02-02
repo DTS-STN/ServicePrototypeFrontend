32fab6406c44f99f195a560e39df4812d4095a91
Yasmin Shash <yshash@deloitte.ca>
2021-02-02 15:25:10 -0500

    402 adding case resource types

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { language } from "./language";
 import { benefits } from "./benefits";
 import { lifejourneys } from "./lifejourneys";
+import { cases } from "./cases";
 import { combineReducers } from "redux";
 
 export const rootReducer = combineReducers({
   language,
   benefits,
+  cases,
   lifejourneys,
 });
