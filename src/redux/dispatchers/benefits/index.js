4ce93709a8f921a8874fb9b213d7564b73436875
Moro-Code <omar@omarnasr.ca>
2021-02-03 17:13:39 -0500

    client redirect

--- src/redux/dispatchers/benefits/index.js
+++ src/redux/dispatchers/benefits/index.js
@@ -2,3 +2,4 @@ export { getBenefits } from "./requestBenefits";
 export { getBenefit } from "./requestBenefit";
 export { getBenefitsCount } from "./requestBenefitsCount";
 export { applyForBenefit } from "./applyForBenefit";
+export { getClientDash } from "./requestClientDash";
