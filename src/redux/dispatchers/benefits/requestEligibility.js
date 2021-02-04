55a434f059f9f8c010f3022d2b7f85e1192ffd59
Thomas Dacey <DaceyTom2@gmail.com>
2021-02-04 13:36:04 -0500

    Added eligility dispatcher

---
+++ src/redux/dispatchers/benefits/requestEligibility.js
@@ -0,0 +1,74 @@
+import "cross-fetch/polyfill";
+import {
+  networkRequestActionCreator,
+  networkRequestFailedActionCreator,
+  NETWORK_REQUEST_TYPES,
+  NETWORK_FAILED_REASONS,
+} from "../../actions";
+import { RESOURCE_TYPES } from "../resourceTypes";
+import { BENEFITSERVICE_URL } from "../../../variables";
+
+async function getEligibility(dispatch, answers) {
+  let response;
+  try {
+    dispatch(
+      networkRequestActionCreator(
+        RESOURCE_TYPES.ELIGIBILITY,
+        NETWORK_REQUEST_TYPES.POST,
+        undefined,
+        {
+          answers,
+        }
+      )
+    );
+    response = await fetch(BENEFITSERVICE_URL + "/benefits/eligible", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        answers,
+      }),
+    });
+  } catch (e) {
+    return dispatch(
+      networkRequestFailedActionCreator(
+        RESOURCE_TYPES.ELIGIBILITY,
+        NETWORK_REQUEST_TYPES.POST,
+        NETWORK_FAILED_REASONS.NO_NETWORK,
+        {
+          message: "Could not connect to benefit service to get eligibility",
+        }
+      )
+    );
+  }
+  if (response.status === 200) {
+    return response;
+  } else {
+    let textData;
+    let data;
+    try {
+      textData = await response.text();
+      data = JSON.parse(textData);
+    } catch (e) {
+      data = textData || "";
+    }
+    return dispatch(
+      networkRequestFailedActionCreator(
+        RESOURCE_TYPES.ELIGIBILITY,
+        NETWORK_REQUEST_TYPES.POST,
+        NETWORK_FAILED_REASONS[response.status] ||
+          NETWORK_FAILED_REASONS.INTERNAL_SERVER_ERROR,
+        typeof data === "string" ? { message: data } : data
+      )
+    );
+  }
+}
+
+/**
+ * dispatch function which applies for a single benefit based on benefit type
+
+ */
+export function requestEligibility(answers) {
+  return (dispatch) => getEligibility(dispatch, answers);
+}
