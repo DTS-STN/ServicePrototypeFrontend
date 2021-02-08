b3e4c90de0d9e7a1f26328cb1acd229f19c0f233
will0684 <jordan.walcaraz@gmail.com>
2021-02-08 09:17:21 -0500

    format user information for curam pre screening

--- src/redux/reducers/answers/answersReducer.js
+++ src/redux/reducers/answers/answersReducer.js
@@ -1,4 +1,5 @@
 import { ACTION_TYPES } from "../../actions";
+import { RESOURCE_TYPES } from "../../dispatchers";
 
 export const answers = function (state = {}, action) {
   switch (action.type) {
@@ -13,6 +14,21 @@ export const answers = function (state = {}, action) {
       };
       delete data[action.key];
       return data;
+    case ACTION_TYPES.NETWORK_RECEIVED:
+      if (action.resourceType === RESOURCE_TYPES.USER_DATA) {
+        let newState = {
+          ...state,
+          person: {},
+        };
+        for (let [key, value] of Object.entries(action.body)) {
+          key = key.replace("person", "");
+          key = key.charAt(0).toLowerCase() + key.slice(1);
+          newState.person[key] = value;
+        }
+        return newState;
+      } else {
+        return state;
+      }
     default:
       return state;
   }
