75246837cdf581b6faa1909ac99278c9e3d114db
Mr. Pond <32133001+P0NDER0SA@users.noreply.github.com>
2021-02-10 11:04:28 -0500

    Update variables.js

--- src/variables.js
+++ src/variables.js
@@ -4,6 +4,10 @@ export const ENVIRONMENT = process.env.NODE_ENV;
 let benefitservice_env_url = process.env.REACT_APP_BENEFITSERVICE_BASE_URL;
 let caseservice_env_url = process.env.REACT_APP_CASESERVICE_BASE_URL;
 
+console.log("ENV VARS");
+console.log(process.env.REACT_APP_BENEFITSERVICE_BASE_URL);
+console.log(process.env.REACT_APP_CASESERVICE_BASE_URL);
+
 if (!benefitservice_env_url && ENVIRONMENT !== "production") {
   //  benefitservice_env_url = "http://localhost:1337";
   // this uses the published version of strapi until we get the benefit service working.
@@ -31,11 +35,13 @@ let keycloak_env_clientId = process.env.REACT_APP_KEYCLOAK_CLIENT_ID;
 let curam_prescreen_link = process.env.REACT_APP_CURAM_PRESCREEN_LINK;
 
 export const BENEFITSERVICE_URL = benefitservice_env_url;
-console.log(BENEFITSERVICE_URL);
 export const CASESERVICE_URL = caseservice_env_url;
-console.log(CASESERVICE_URL);
 export const KEYCLOAK_URL = keycloak_env_url;
 export const KEYCLOAK_REALM = keycloak_env_realm;
 export const KEYCLOAK_CLIENT_ID = keycloak_env_clientId;
 export const USER_SERVICE_URL = user_service_env_url;
 export const CURAM_PRESCREEN_LINK = curam_prescreen_link;
+
+console.log("EXPORTED VARS");
+console.log(BENEFITSERVICE_URL);
+console.log(CASESERVICE_URL);
