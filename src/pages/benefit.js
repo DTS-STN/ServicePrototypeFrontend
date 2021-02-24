5283b1e5e2794c1ae592b92dbfed75a1e5d3b0e6
Thomas Dacey <DaceyTom2@gmail.com>
2021-02-24 08:53:25 -0500

    answers are saved before going to keycloak

--- src/pages/benefit.js
+++ src/pages/benefit.js
@@ -143,6 +143,7 @@ export function BenefitPage() {
 
   const displayEntitlementTable = () => {
     if (!keycloak.authenticated) {
+      saveAnswers(answers);
       keycloak.login();
     }
     console.log(entitlement);
